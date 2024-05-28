455900499555285c3364d6f56eebf1986ce1573a
rsap4you <kumaramarjeetraj71@gmail.com>
2024-05-28 13:51:00 +0530

    first commit

--- src/config/config.js
+++ src/config/config.js
@@ -18,14 +18,25 @@ export const navs = [
 ];
 
 export const router = [
-  { path: "/", key: "general", category: "general", country: "us" },
-  { path: "/categories/general", key: "general", category: "general", country: "us" },
-  { path: "/categories/business", key: "business", category: "business", country: "us" },
-  { path: "/categories/sports", key: "sports", category: "sports", country: "us" },
-  { path: "/categories/science", key: "science", category: "science", country: "us" },
-  { path: "/categories/health", key: "health", category: "health", country: "us" },
-  { path: "/categories/entertainment", key: "entertainment", category: "entertainment",country: "us" },
-  { path: "/categories/technology", key: "technology", category: "technology", country: "us" }
+  // { path: "/", key: "general", category: "general", country: "us" },
+  // { path: "/categories/general", key: "general", category: "general", country: "us" },
+  // { path: "/categories/business", key: "business", category: "business", country: "us" },
+  // { path: "/categories/sports", key: "sports", category: "sports", country: "us" },
+  // { path: "/categories/science", key: "science", category: "science", country: "us" },
+  // { path: "/categories/health", key: "health", category: "health", country: "us" },
+  // { path: "/categories/entertainment", key: "entertainment", category: "entertainment",country: "us" },
+  // { path: "/categories/technology", key: "technology", category: "technology", country: "us" }
+
+
+
+  { path: "/", key: "general", category: "general", country: "in" },
+  { path: "/categories/general", key: "general", category: "general", country: "in" },
+  { path: "/categories/business", key: "business", category: "business", country: "in" },
+  { path: "/categories/sports", key: "sports", category: "sports", country: "in" },
+  { path: "/categories/science", key: "science", category: "science", country: "in" },
+  { path: "/categories/health", key: "health", category: "health", country: "in" },
+  { path: "/categories/entertainment", key: "entertainment", category: "entertainment",country: "in" },
+  { path: "/categories/technology", key: "technology", category: "technology", country: "in" }
 ];
 
 export const summary = "Channel and PublishedAt";
